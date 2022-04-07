f324a6a8b8c9f22d80b7e402bf01d17783a14f37
ljh5973 <ljh6650@naver.com>
2022-04-07 10:37:59 +0900

    커밋

--- app/app.js
+++ app/app.js
@@ -6,17 +6,18 @@ const express =require("express");
 const app = express();
 
 
-const PORT=3000;
+
 // 라우팅
-const home=require("./routes/home/index");
+const home=require("./src/routes/home");
 
 
 // 앱 세팅
 
-app.set("views","./views");
+app.set("views","./src/views");
 app.set("view engine", "ejs");
 
-
+app.use(express.static(`${__dirname}/src/public`));
 app.use("/", home);  // use -> 미들 웨어를 등록해주는 메서드.
 
 module.exports=app;
+
