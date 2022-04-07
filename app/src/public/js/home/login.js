f9c603c85d267dab57071ce1177b6d96365aec20
ljh5973 <ljh6650@naver.com>
2022-04-07 10:51:29 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,23 @@
 "use strict"
 
-console.log("hello")
+const id = document.querySelector("#id"),
+    pw= document.querySelector("#pw"),
+    loginBtn=document.querySelector("#loginBtn")
+
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req ={
+        id: id.value,
+        pw: pw.value
+    };
+
+    fetch("/login", {
+        method:"POST",
+        header:{
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(req)
+    })
+};
