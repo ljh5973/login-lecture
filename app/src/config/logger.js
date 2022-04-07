b77d9dab7ed988013f9e093b831cf4e9a0c804da
ljh5973 <ljh6650@naver.com>
2022-04-07 23:30:35 +0900

    롷그 관리 모듈 winston 등록 및 설정

---
+++ app/src/config/logger.js
@@ -0,0 +1,46 @@
+const { transport } = require("winston");
+const {createLogger, transports, format} = require("winston");
+const {combine, timestamp,printf,label, simple, colorize}= format;
+const printFormat=   printf(({timestamp,label, level, message})=>{
+    return `${timestamp} [${label}] ${level} : ${message}`
+});
+
+
+const printLogformat={
+    file:combine(
+    label({
+        label:"백엔드 맛보기"
+    }),
+    timestamp({
+        format:"YYYY-MM-DD"
+    }),
+    printFormat
+),
+    console: combine(
+        colorize(), //
+        simple()
+)
+};
+
+const opts={
+    file:new transports.File({
+        filename:"access.logger",
+        dirname:"./logs",
+        level:"info",
+        format:printLogformat.file,
+    }),
+    console:  new transports.Console({
+        level:"info",
+        format:printLogformat.console,
+    })
+}
+
+const logger=createLogger({
+    transports: [opts.file
+    ]
+});
+
+if(process.env.NODE_ENV !=="production"){
+    logger.add(opts.console);
+}
+module.exports=logger;
